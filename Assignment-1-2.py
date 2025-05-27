c1eef5aa5a9e95607ec8147ad220a46f3c1a4b43
zainab abdulhasan <zandulstudent@gmail.com>
2025-05-26 17:49:10 -0700

    Add Assignment 1: STEM Center Temperature Project

---
+++ Assignment-1-2.py
@@ -0,0 +1,28 @@
+"""
+Assigment 1: Opening Lines
+Submitted by Zainab Abdulhasan
+Submitted: September 29, 2024
+
+Assigment 1: This program demonstrates printing lines of text to the screen
+"""
+
+
+def print_header():
+    print("STEM Center Temperature Project")
+    print("Zainab Abdulhasan")
+
+
+def main():
+    print_header()
+
+if __name__ == "__main__":
+    main()
+
+
+r"""
+
+C:\\Users\\zandu.LAPTOP-37DTINUF\\AppData\\Local\\Programs\\Python\\Python312\\python.exe "C:\Users\\zandu.LAPTOP-37DTINUF\\OneDrive\\Documents\\phython projects\\Assignment-1.py"
+
+"""
+
+
